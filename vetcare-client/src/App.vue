d5a32c76a489c365a16f84e0480ddf45ebf5fed8
amezquitam <amezquitamiguel7@gmail.com>
2024-05-27 10:36:51 -0500

    vetcare-client

---
+++ vetcare-client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+};
+</script>
+
+<style>
+/* Puedes agregar estilos globales aquí si es necesario */
+</style>
---
+++ vetcare-client/src/components/HomePage.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="home">
+        <header class="header">
+            <div class="headermodif">
+                <div class="logo">
+                    <img src="../resource/img/Logo-VetCare-invertido.png" alt="VetCare Logo" class="imglogo" />
+                </div>
+                <h1 class="logo-text">VetCare</h1>
+            </div>
+            <div class="nav-signup">
+                <nav>
+                    <ul>
+                        <li><a href="#">INICIO</a></li>
+                        <li><a href="#">BLOG</a></li>
+                        <li><a href="#">PETSHOP</a></li>
+                    </ul>
+                </nav>
+                <div class="btn-signup">
+                    <button class="signup">Sign up</button>
+                </div>
+            </div>
+        </header>
+
+        <div class="IntroWelcome">
+            <section class="intro">
+                <h2>En vetcare <br /> encontraras:</h2>
+
+                <p>
+                    Atención médica virtual.<br />
+                    Consejos de cuidado de mascotas.<br />
+                    Información sobre enfermedades comunes.
+                </p>
+            </section>
+            <section class="welcome">
+                <img src="../resource/img/ilustracion.png" alt="Welcome" />
+            </section>
+        </div>
+
+        <main class="main-content">
+            <section class="information">
+                <div class="footer-links">
+                    <div class="Somos">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <a href="#" class="aSomos">¿Quienes somos?</a>
+                    </div>
+                    <div class="Reseñas">
+                        <i class="fa-regular fa-star"></i>
+                        <a href="#" class="aReseña">Reseñas</a>
+                    </div>
+                    <div class="Cont">
+                        <i class="fa-solid fa-file-arrow-down"></i>
+                        <a href="#" class="aCont">Contenido gratuito</a>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <footer class="footer">
+
+            <div class="social-media">
+                <a href="#"><i class="fa-brands fa-instagram fa-2xl" alt="InstagramProfile"></i> </a>
+                <a href="#"><i class="fa-brands fa-whatsapp fa-2xl" alt="WhatsappProfile"></i></a>
+                <a href="#"><i class="fa-brands fa-github fa-2xl" alt="GitHubprofile"></i></a>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script src="../resource/js/HomePage.js"></script>
+
+<style src="../resource/css/HomePage.css"></style>
