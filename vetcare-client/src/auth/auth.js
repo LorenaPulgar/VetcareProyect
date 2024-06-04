928c5625f558c66dbc8887fb6c3d826d65beef12
amezquitam <amezquitamiguel7@gmail.com>
2024-06-04 07:42:19 -0500

    global exception handling

--- vetcare-client/src/auth/auth.js
+++ vetcare-client/src/auth/auth.js
@@ -1,27 +1,51 @@
 import axios from "axios";
 
 export async function login(email, password) {
-    const response = await axios.post('http://localhost:8090/auth/login', {
-        email,
-        password
-    })
+    try {
 
-    const responseBody = response.data
-
-    saveToken(responseBody.token)
+        const response = await axios.post('http://localhost:8090/auth/login', {
+            email,
+            password
+        })
+        
+        const responseBody = response.data
+        
+        saveToken(responseBody.token)
+        
+        alert(":)")
+        
+    } catch(err) {
+        if (err.response && err.response.status === 404) {
+            if (err.response.data && err.response.data.description) {
+                alert(err.response.data.description)
+            }
+        }
+    }
 }
 
 export async function register({ name, email, password, phoneNumber }) {
-    const response = await axios.post('http://localhost:8090/auth/register', {
-        name,
-        email,
-        password,
-        phoneNumber
-    })
+    try {
 
-    const responseBody = response.data
+        const response = await axios.post('http://localhost:8090/auth/register', {
+            name,
+            email,
+            password,
+            phoneNumber
+        })
+        
+        const responseBody = response.data
+        
+        saveToken(responseBody.token)
+        
+        alert(":)")
+    } catch(err) {
+        if (err.response && err.response.status === 409) {
+            if (err.response.data && err.response.data.description) {
+                alert(err.response.data.description)
+            }
+        }
+    }
 
-    saveToken(responseBody.token)
 }
 
 function saveToken(token) {
--- vetcare-server/src/main/java/com/example/vetcare/auth/AuthService.java
+++ vetcare-server/src/main/java/com/example/vetcare/auth/AuthService.java
@@ -1,10 +1,13 @@
 package com.example.vetcare.auth;
 
 import com.example.vetcare.enums.Role;
+import com.example.vetcare.exception.AlreadyExistException;
+import com.example.vetcare.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import com.example.vetcare.entity.User;
 import com.example.vetcare.jwt.JwtService;
 import com.example.vetcare.repository.UserRepository;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,14 +31,20 @@ public class AuthService {
                 .role(Role.USER)
                 .build();
 
-        userRepository.save(user);
+        try {
+            userRepository.save(user);
+
+        } catch (DataIntegrityViolationException err) {
+            throw new AlreadyExistException("This email is already taken");
+        }
 
         return new AuthResponse(jwtService.getToken(user));
     }
 
     public AuthResponse login(LoginRequest request) {
+        UserDetails userDetails = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new ResourceNotFoundException("Invalid email or password"));
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
-        UserDetails userDetails = userRepository.findByEmail(request.email()).orElseThrow();
         String token = jwtService.getToken(userDetails);
         return new AuthResponse(token);
     }
